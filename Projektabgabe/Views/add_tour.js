368c81bc22285bb7dabe458a5e0e699766cc9099
Antonia <antonia.jost@web.de>
2021-07-21 15:36:03 +0200

    update

--- Projektabgabe/Views/add_tour.js
+++ Projektabgabe/Views/add_tour.js
@@ -2,14 +2,23 @@
 var center = [51.961237, 7.625187];
 
 // Erstellung einer Variablen, die die Karte enthält, initial settings
-var map = L.map('map',).setView(center, 13.5); 
+var karte = L.map('karte',).setView(center, 13.5); 
 
 // MapTiler hinuzfügen
 L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=86IucIQ0W7mo5uspiDDB', 
     {
      attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
-    }).addTo(map); 
+    }).addTo(karte); 
 
+// Draw Group/ Toolbar hinzufügen
+var drawnItems = new L.FeatureGroup();
+karte.addLayer(drawnItems);
+var drawControl = new L.Control.Draw ({
+    edit: {
+        featureGroup: drawnItems
+    }
+});
+karte.addControl(drawControl);
     
 
 
