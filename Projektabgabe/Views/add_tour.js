da5a55524afb75fe9d4895c13b0ef121c1381148
Antonia <antonia.jost@web.de>
2021-07-21 18:40:25 +0200

    Update

--- Projektabgabe/Views/add_tour.js
+++ Projektabgabe/Views/add_tour.js
@@ -2,7 +2,7 @@
 var center = [51.961237, 7.625187];
 
 // Erstellung einer Variablen, die die Karte enthält, initial settings
-var karte = L.map('karte',).setView(center, 13.5); 
+var karte = L.map('karte',).setView(center, 15); 
 
 // MapTiler hinuzfügen
 L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=86IucIQ0W7mo5uspiDDB', 
