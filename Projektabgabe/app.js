1b3c34f66ba93faf94fd8a6701612c595b752f3e
Antonia <antonia.jost@web.de>
2021-07-22 17:38:49 +0200

    Update app.js

--- Projektabgabe/app.js
+++ Projektabgabe/app.js
@@ -3,8 +3,6 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
 // adding routers
 var indexRouter = require('./routes/0_index.js');
