c0d6df68b440e6dbe403574db451b39165cf371e
Antonia <antonia.jost@web.de>
2021-07-27 13:06:05 +0200

    update

--- Projektabgabe/routes/0_index.js
+++ Projektabgabe/routes/0_index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET add page. */
+/* GET index page. */
 router.get('/', function(req, res, next) {
   res.render('0_index', { title: 'Homepage' });
 });
