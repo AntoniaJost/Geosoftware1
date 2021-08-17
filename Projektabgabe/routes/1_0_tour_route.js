7b6a711abde4024fcb42bc38e49b10ee01f2c0ce
Antonia <antonia.jost@web.de>
2021-08-17 18:38:28 +0200

    update

--- Projektabgabe/routes/1_0_tour_route.js
+++ Projektabgabe/routes/1_0_tour_route.js
@@ -6,21 +6,27 @@ const assert = require('assert')
 const url = 'mongodb://localhost:27017' // connection URL
 const client = new MongoClient(url) // create mongodb client
 
-router.get('/', function(req, res, next)
+
+
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("Stadttour");
+    //Find all documents in the customers collection:
+    dbo.collection("neueTouren").find({}).toArray(function(err, result) {
+      if (err) throw err;
+      console.log(result);
+      db.close();
+    });
+  });
+    
+
+  router.get('/', function(req, res, next)
 {
     res.render("1_tour");
-    var resultArray = [];
-    client.connect(url, function(err, db){
-        assert.equal(null, err);
-        var cursor = db.collection('touren').find();
-        cursor.forEach(function (doc, err) {
-            assert.equal(null,err);
-            resultArray.push(doc);
-        }, function () {
-            db.close();
-            res.render('1_tour', {items: resultArray})
-        });
-    });
+    res.json(result);
+    res.send(result);
+    res.render(result);
 });     
+
 
 module.exports = router; 
