e4b8a4d8c4314771692fc2ee57f0c233d21daa84
Antonia <antonia.jost@web.de>
2021-08-18 19:42:46 +0200

    display mongoDB content

--- Projektabgabe/routes/1_0_tour_route.js
+++ Projektabgabe/routes/1_0_tour_route.js
@@ -7,29 +7,43 @@ const url = 'mongodb://localhost:27017' // connection URL
 const client = new MongoClient(url) // create mongodb client
 
 
-
-//Connect in die get Anfrage
 // res.render(..) als callback der find-Methode
+// get Endpunkt??
+// asyn/ await??
+// nur einmal res.render/send verwenden, sonst Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to
+// -> aber wie dann 1_tour & und result senden??
 
-MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    var dbo = db.db("Stadttour");
-    //Find all documents in the customers collection:
-    dbo.collection("neueTouren").find({}).toArray(function(err, result) {
-      if (err) throw err;
-      console.log(result);
-      db.close();
-    });
-  });
-    
+router.get('/', function(req, res, next)
+{
+
+    res.render("1_tour");
+    /*res.json(result);
+    res.send(result);
+    res.render(result);*/
+});       
 
   router.get('/', function(req, res, next)
 {
-    res.render("1_tour");
-    res.json(result);
-    res.send(result);
-    res.render(result);
-});     
+
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var touren = db.db("Stadttour");
+    //Find all documents in the customers collection:
+    touren.collection("Stadttour").find({}.toArray(function(err, result) {
+      if (err) {
+        throw err;
+      } else if (result.length) {
+          console.log(result);
+          res.send(result);
+      } else {
+          res.send('No documents found');
+      }
+      //console.log(result);
+      db.close();
+    }));
+  });
+
+});
 
 
 module.exports = router; 
