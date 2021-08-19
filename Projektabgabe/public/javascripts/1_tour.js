013e3cccf2dc7961129cea6228b97da9bd9be483
Antonia <antonia.jost@web.de>
2021-08-19 21:00:48 +0200

    update

--- Projektabgabe/public/javascripts/1_tour.js
+++ Projektabgabe/public/javascripts/1_tour.js
@@ -1,26 +1,26 @@
-// Mitte der Karte
-var center = [51.961237, 7.625187];
+let ausgangssituation = fetch ('/tour')
+    .then (ausgangssituation => {
+        if (ausgangssituation.ok) {
+            // Mitte der Karte
+            var center = [51.961237, 7.625187];
 
-// Erstellung einer Variablen, die die Karte enthält, initial settings
-var map = L.map('map',).setView(center, 13.5); 
+            // Erstellung einer Variablen, die die Karte enthält, initial settings
+            var map = L.map('map',).setView(center, 13.5); 
 
-// MapTiler hinuzfügen
-L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=86IucIQ0W7mo5uspiDDB', 
-    {
-     attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
-    }).addTo(map); 
-
-let response = fetch('/:routeID')
-    .then(response => {
-        if (response.ok) {
-            response.json().then(data => console.log(data));
+            // MapTiler hinuzfügen
+            L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=86IucIQ0W7mo5uspiDDB', 
+                {
+                attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+                }).addTo(map); 
         } else {
-            console.log('Es gibt einen Fehler');
+            console.log('Fehler 1.Art');
         }
-    });
-//alert (routeLayer);
+    })
 
-$('#dropdown-demo li').on('click', function(){
-    $('#dropdownMenuButton1').val($(this).text());
-}); 
-//var routeLayer = L.geoJSON(routeID).addTo(map);
+
+// warum auch immer, geht er hier nicht rein...
+let response = fetch('tour/:routeID')
+    .then(response => response.json()).then(response => {
+        L.geoJson(response).addTo(map);
+    });
+
--- Projektabgabe/routes/1_0_tour_route.js
+++ Projektabgabe/routes/1_0_tour_route.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var router = express.Router(); 
 const MongoClient = require('mongodb').MongoClient
+const mongo = require('mongodb')
 const assert = require('assert')
 
 const url = 'mongodb://localhost:27017' // connection URL
@@ -20,9 +21,21 @@ router.get('/', async function(req, res, next)
 
 });
 
-router.get("/:routeID", (req,res,next) => {
+router.get('/:routeID', async (req,res,next) => {
+  
+  await client.connect();
+
   routeID = req.params.routeID;
-  res.send(routeID);
+
+  var touren = client.db("Stadttour");
+
+  let documents = await touren.collection("neueTouren").findOne({"_id" : new mongo.ObjectId(routeID)});
+
+  console.log(documents);
+
+  res.send(documents); 
+  //res.render("1_tour", {tours: documents}) //-> error cannot read property of undefinded
+  
 })
 
 module.exports = router; 
